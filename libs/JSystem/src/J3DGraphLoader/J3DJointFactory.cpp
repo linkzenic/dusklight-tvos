cc268989725bb05a29834a07ce33a79f7af6a049
PJB3005 <pieterjan.briers+git@gmail.com>
2026-03-02 20:00:46 +0100

    Animation/joint loading BE fixes

--- libs/JSystem/src/J3DGraphLoader/J3DJointFactory.cpp
+++ libs/JSystem/src/J3DGraphLoader/J3DJointFactory.cpp
@@ -8,6 +8,22 @@
 J3DJointFactory::J3DJointFactory(J3DJointBlock const& block) {
     mJointInitData = JSUConvertOffsetToPtr<J3DJointInitData>(&block, (uintptr_t)block.mpJointInitData);
     mIndexTable = JSUConvertOffsetToPtr<u16>(&block, (uintptr_t)block.mpIndexTable);
+
+#if TARGET_LITTLE_ENDIAN
+    for (int i = 0; i < block.mJointNum; i++) {
+        auto& index = mIndexTable[i];
+        be_swap(index);
+
+        auto initData = &mJointInitData[index];
+        be_swap(initData->mKind);
+        be_swap(initData->mTransformInfo.mScale);
+        be_swap(initData->mTransformInfo.mRotation);
+        be_swap(initData->mTransformInfo.mTranslate);
+        be_swap(initData->mRadius);
+        be_swap(initData->mMin);
+        be_swap(initData->mMax);
+    }
+#endif
 }
 
 J3DJoint* J3DJointFactory::create(int no) {

a46de3daa3cef87fdd39f1521eb72443661bfe41
MelonSpeedruns <melonspeedruns@stratobox.net>
2026-03-31 13:45:53 -0400

    fix reticle position

--- src/d/actor/d_a_player.cpp
+++ src/d/actor/d_a_player.cpp
@@ -402,11 +402,12 @@ void daPy_sightPacket_c::setSight() {
     
     #if TARGET_PC
     mDoLib_project(&mPos, &proj, {0, 0, FB_WIDTH, FB_HEIGHT});
+    mDoMtx_stack_c::transS(proj.x - 16.0f, proj.y - 8.0f, proj.z);
     #else
     mDoLib_project(&mPos, &proj);
+    mDoMtx_stack_c::transS(proj.x, proj.y, proj.z);
     #endif
 
-    mDoMtx_stack_c::transS(proj.x, proj.y, proj.z);
     mDoMtx_stack_c::scaleM(32.0f, 32.0f, 32.0f);
     mDoMtx_copy(mDoMtx_stack_c::get(), mProjMtx);
     dComIfGd_set2DXlu(this);

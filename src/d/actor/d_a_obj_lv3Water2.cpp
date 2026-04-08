374ed13308bc6e14ec3ff156a3b5b3b1318ddc04
MelonSpeedruns <melonspeedruns@stratobox.net>
2026-04-08 18:37:50 -0400

    Fix line indent

--- src/d/actor/d_a_obj_lv3Water2.cpp
+++ src/d/actor/d_a_obj_lv3Water2.cpp
@@ -202,8 +202,7 @@ int daLv3Water2_c::Draw() {
             C_MTXLightPerspective(lightProjMtx, dComIfGd_getView()->fovy,
                                   dComIfGd_getView()->aspect, 1.0f, 1.0f, dusk::getSettings().game.useWaterProjectionOffset ? -0.01f : 0.0f, 0);
             #else
-            C_MTXLightPerspective(lightProjMtx, dComIfGd_getView()->fovy,
-                                  dComIfGd_getView()->aspect, 1.0f, 1.0f, -0.01f, 0);
+            C_MTXLightPerspective(lightProjMtx, dComIfGd_getView()->fovy, dComIfGd_getView()->aspect, 1.0f, 1.0f, -0.01f, 0);
             #endif
 
             #if WIDESCREEN_SUPPORT

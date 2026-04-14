e5f70feb2154344a108367a188914d5b47068683
CraftyBoss <talibabdulmaalik@gmail.com>
2026-04-14 03:07:47 -0700

    fix compilation error
    GlyphTextures no longer exists

--- libs/JSystem/include/JSystem/JUtility/JUTResFont.h
+++ libs/JSystem/include/JSystem/JUtility/JUTResFont.h
@@ -69,11 +69,7 @@ public:
     // some types uncertain, may need to be fixed
     /* 0x1C */ int mWidth;
     /* 0x20 */ int mHeight;
-#if TARGET_PC
-    GlyphTextures* mGlyphTextures;
-#else
     /* 0x24 */ TGXTexObj mTexObj;
-#endif
     /* 0x44 */ int mTexPageIdx;
     /* 0x48 */ const ResFONT* mResFont;
     /* 0x4C */ ResFONT::INF1* mInf1Ptr;

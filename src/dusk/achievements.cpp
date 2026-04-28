c869c99dc3fa5a122dd4b25baed06b99f775396e
Julian Maynes <thelastpocket@gmail.com>
2026-04-28 07:12:29 -0700

    Add iron boots achievement

--- src/dusk/achievements.cpp
+++ src/dusk/achievements.cpp
@@ -379,7 +379,7 @@ std::vector<AchievementSystem::Entry> AchievementSystem::makeEntries() {
         },
         {
             {
-                "email-me",
+                "email_me",
                 "Email Me",
                 "Read a letter during the Dark Beast Ganon fight.",
                 AchievementCategory::Misc,
@@ -392,7 +392,27 @@ std::vector<AchievementSystem::Entry> AchievementSystem::makeEntries() {
                 }
             },
             {}
+        },
+        {
+            {
+                "heavy-hitter",
+                "Heavy Hitter",
+                "Wear the Iron Boots during the end credits.",
+                AchievementCategory::Misc,
+                false, 0, 0, false
+            },
+            [](Achievement& a, json&) {
+                const auto* link = static_cast<const daAlink_c*>(daPy_getPlayerActorClass());
+                if (link == nullptr || link->mProcID != daAlink_c::PROC_GANON_FINISH) {
+                    return;
+                }
+                if (daPy_getPlayerActorClass()->checkEquipHeavyBoots()) {
+                    a.progress = 1;
+                }
+            },
+            {}
         }
+
     };
 }
 

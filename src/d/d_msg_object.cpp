89c7ce6408087325ae3c2f36bc98b20939f6f772
TakaRikka <takarikka@outlook.com>
2026-05-05 04:44:14 -0700

    fix mirror mode text / rodeo direction

--- src/d/d_msg_object.cpp
+++ src/d/d_msg_object.cpp
@@ -427,6 +427,16 @@ static void dummyStrings() {
 dMsgObject_HIO_c g_MsgObject_HIO_c;
 
 int dMsgObject_c::_execute() {
+#if TARGET_PC
+    if (dusk::getSettings().game.enableMirrorMode) {
+        // enable wii message index override
+        g_MsgObject_HIO_c.mMessageDisplay = 1;
+    } else if (!dusk::getSettings().game.enableMirrorMode && g_MsgObject_HIO_c.mMessageDisplay == 1) {
+        g_MsgObject_HIO_c.mMessageDisplay = 0;
+    }
+#endif
+
+
     field_0x4c7 = 0;
     if (mpTalkHeap != NULL) {
         field_0x148 = mDoExt_setCurrentHeap(mpTalkHeap);

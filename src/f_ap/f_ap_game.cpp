cf6a90d13c82777220925f930647238f420c2546
MelonSpeedruns <melonspeedruns@outlook.com>
2026-05-10 11:32:55 -0400

    Fix recording mode muting music until next reboot (#832)
    Co-authored-by: MelonSpeedruns <melonspeedruns@stratobox.net>

--- src/f_ap/f_ap_game.cpp
+++ src/f_ap/f_ap_game.cpp
@@ -740,6 +740,8 @@ static void fapGm_AfterRecord() {
     fapGm_After();
 }
 
+BOOL isRecording = false;
+
 static void duskExecute() {
     handleGamepadColor();
     updateAutoSave();
@@ -747,6 +749,11 @@ static void duskExecute() {
     if (dusk::getSettings().game.recordingMode) {
         Z2GetSoundMgr()->getSeqMgr()->getParams()->moveVolume(0.0f, 0);
         Z2GetSoundMgr()->getStreamMgr()->getParams()->moveVolume(0.0f, 0);
+        isRecording = true;
+    } else if (isRecording) {
+        Z2GetSoundMgr()->getSeqMgr()->getParams()->moveVolume(1.0f, 0);
+        Z2GetSoundMgr()->getStreamMgr()->getParams()->moveVolume(1.0f, 0);
+        isRecording = false;
     }
 
     if (mDoCPd_c::getHoldR(PAD_1) && mDoCPd_c::getTrigX(PAD_1)) {

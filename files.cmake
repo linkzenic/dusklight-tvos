b2a4d4fb6f10de7cf999c4545de931490029b47c
Linkzenic <linkzenic@users.noreply.github.com>
2026-08-01 22:58:41 -0700

    Include touch camera service in tvOS release

--- files.cmake
+++ files.cmake
@@ -1485,11 +1485,27 @@ set(DUSK_FILES
         src/dusk/mods/log_buffer.hpp
         src/dusk/mods/manifest.cpp
         src/dusk/mods/manifest.hpp
+        src/dusk/hud_layout.cpp
+        src/dusk/hud_layout.hpp
+        src/dusk/mods/svc/stage_flow.cpp
+        src/dusk/mods/svc/stage_flow.hpp
         src/dusk/mods/svc/camera.cpp
+        src/dusk/mods/svc/action_input.cpp
+        src/dusk/mods/svc/action_input.hpp
         src/dusk/mods/svc/config.cpp
         src/dusk/mods/svc/config.hpp
+        src/dusk/mods/svc/file_select.cpp
+        src/dusk/mods/svc/file_select.hpp
         src/dusk/mods/svc/game.cpp
         src/dusk/mods/svc/gfx.cpp
+        src/dusk/mods/svc/hud_layout.cpp
+        src/dusk/mods/svc/hud_layout.hpp
+        src/dusk/mods/svc/aim_control.cpp
+        src/dusk/mods/svc/aim_control.hpp
+        src/dusk/mods/svc/item_assignment.cpp
+        src/dusk/mods/svc/item_assignment.hpp
+        src/dusk/mods/svc/midna_dialog.cpp
+        src/dusk/mods/svc/midna_dialog.hpp
         src/dusk/mods/svc/hook.cpp
         src/dusk/mods/svc/host.cpp
         src/dusk/mods/svc/log.cpp
